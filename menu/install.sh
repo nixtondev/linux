2bff21434fd3cd401f5fb8dc1cd74797d04703ed
~$  The Coder ( nixton ) <83667327+nixtondev@users.noreply.github.com>
2023-11-08 14:39:38 +0100

    Add files via upload

---
+++ menu/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+mkdir Accessibility
+
+mkdir Development
+
+mkdir Education
+
+mkdir Games
+
+mkdir Graphics
+
+mkdir Pictures
+
+mkdir Internet
+
+mkdir Music
+
+mkdir Video
+
+mkdir Office
+
+mkdir Security
+
+mkdir Utilities
