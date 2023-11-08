8566578c5c5881282b70a59fa31ef0bc005085ed
~$  The Coder ( nixton ) <83667327+nixtondev@users.noreply.github.com>
2023-11-08 12:37:33 +0100

    Add files via upload

---
+++ alfabetet/install.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+#A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z.
+
+mkdir app && cd app
+
+mkdir A
+
+mkdir B
+
+mkdir C
+
+mkdir D
+
+mkdir E
+
+mkdir F
+
+mkdir G
+
+mkdir H
+
+mkdir I
+
+mkdir J
+
+mkdir K
+
+mkdir L
+
+mkdir M
+
+mkdir N
+
+mkdir O
+
+mkdir P
+
+mkdir Q
+
+mkdir R
+
+mkdir S
+
+mkdir T
+
+mkdir U
+
+mkdir V
+
+mkdir W
+
+mkdir X
+
+mkdir Y
+
+mkdir Z
